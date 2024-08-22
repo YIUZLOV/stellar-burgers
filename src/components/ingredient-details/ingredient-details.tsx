c327e920e76055b6526ecad39d8b65f5096f7f87
Evgeniy Uzlov <yiuzlov@yandex.ru>
2024-08-22 21:59:06 +0300

    project completed for review

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
+import { getIngredientSelector } from '../../slices/ingredientsSlice';
+import styles from '../app/app.module.css';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const ingredients = useSelector(getIngredientSelector);
 
+  const { id } = useParams();
+
+  const ingredientData = ingredients.find((item) => item._id === id);
   if (!ingredientData) {
     return <Preloader />;
   }
 
-  return <IngredientDetailsUI ingredientData={ingredientData} />;
+  return (
+    <div className={styles.detailPageWrap}>
+      <IngredientDetailsUI ingredientData={ingredientData} />
+    </div>
+  );
 };
